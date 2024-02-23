5b378332d4e57ed845a317f50ff3e2508f0acae9
isabela.silva <isabela.silva@agi.com.br>
2024-02-23 08:04:19 -0300

    Aula 1- Cria o projeto e implementa classe obter dados
    Cria o projeto com gradle, e implementa a classe obter dados para realizar a request e receber o response.

---
+++ screenmatch2/src/main/java/br/com/alura/screenmatch2/Screenmatch2Application.java
@@ -0,0 +1,25 @@
+package br.com.alura.screenmatch2;
+
+import br.com.alura.screenmatch2.service.ConsumoApi;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Screenmatch2Application implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Screenmatch2Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		ConsumoApi consumoApi = new ConsumoApi();
+		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=56dac46c");
+		System.out.println(json);
+		json = consumoApi.obterDados("https://coffee.alexflipnote.dev/random.json");
+		System.out.println(json);
+
+
+	}
+}
