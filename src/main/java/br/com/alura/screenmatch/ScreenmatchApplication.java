206d47fe96dbacdea854e6103760550ee18a2e87
David <xxdavidxxsilva@gmail.com>
2024-05-05 11:14:28 -0300

    inicio do projeto spring

---
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,22 @@
+package br.com.alura.screenmatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Screenmatch2Application implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Screenmatch2Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		var consumoApi = new ConsumoApi();
+		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=56dac46c");
+		System.out.println(json);
+		ConverteDados converteDados = new ConverteDados();
+		DadosSerie dadosSerie = converteDados.obterDados(json, DadosSerie.class);
+		System.out.println(dadosSerie);
+	}
+}
